0c56a730729cce7817b4c56ecbfbceb20c7b62b5
Cristi <dcsterpu@assystem.com>
2018-02-13 15:43:07 +0200

    Created test structure Added test plan (to be improved)

---
+++ test_connectors.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import filecmp
+
+
+class TestParser(unittest.TestCase):
+    def test_demo(self):
+        os.system("connectors.py in D:\\test\connectors\output out D:\\test\connectors\output")
+        self.assertTrue(filecmp.cmp('D:\\test\connectors\output\Connectors.arxml', 'D:\\test\connectors\Connectors.arxml', shallow=False))
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestParser)
+unittest.TextTestRunner(verbosity=1).run(suite)
